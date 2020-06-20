6a9312d1c728b35fc85bf2badcc26a4c1165d6ff
SamuelEdward <samueledward40@gmail.com>
2020-06-20 08:28:35 -0700

    Opening loop for game

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var c = document.createElement("canvas");
 var ctx = c.getContext("2d");
 c.width = 500;
 c.height = 350;
+document.body.appendChild(c);
 
 function loop() {
-    
+    ctx.fillStyle = "#19f"
+
 }
