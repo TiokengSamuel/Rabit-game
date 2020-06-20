1949cb73f9fd8be9c2c54e360c098d80051d81f1
SamuelEdward <samueledward40@gmail.com>
2020-06-20 09:21:01 -0700

    changing lerp

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ while (perm.length < 255) {
     perm.push(val);
 }
 
-var lerp = (a,b,t) => a + (b-a) * t;
+var lerp = (a,b,t) => a + (b-a) * (1-Math.cos(t*Math.PI)/2);
 var noise = x => {
     x = x * 0.01 % 255;
     return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x.Math.floor(x));
