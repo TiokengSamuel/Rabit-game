2cac00adcc12001eaed140050af64889b5bf79aa
SamuelEdward <samueledward40@gmail.com>
2020-06-20 08:51:42 -0700

    returning lerp

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ while (perm.length < 255) {
 
 var lerp = (a,b,t) => a + (b-a) * t;
 var noise = x => {
-    return
+    return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x.Math.floor(x))
 }
 
 function loop() {
